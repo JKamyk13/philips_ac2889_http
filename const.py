409315c50c3f17ae7173cb7ad03b4297ea0a1b3a
JKamyk13 <juliankamyk13@gmail.com>
2026-09-11 16:08:23 +0000

    0.1.2

--- const.py
+++ const.py
@@ -3,3 +3,24 @@
 DOMAIN = "philips_ac2889_http"
 DEFAULT_NAME = "Philips AC2889"
 DEFAULT_SCAN_INTERVAL = 30
+
+FAN_SPEEDS = ("s", "1", "2", "3", "t", "a")
+SPEED_NAMES = {
+	"s": "Sleep",
+	"1": "Speed 1",
+	"2": "Speed 2",
+	"3": "Speed 3",
+	"t": "Turbo",
+	"a": "Auto",
+}
+
+MODES = ("M", "A")
+MODE_NAMES = {
+	"M": "Manual",
+	"A": "Allergen",
+}
+
+FUNCTIONS = ("P",)
+FUNCTION_NAMES = {
+	"P": "Purification",
+}
